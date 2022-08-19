d3b40393a9e9fb48658550b6b600a0632814732a
Hemanth Reddy Arjunagari <83241138+hemanth12reddy@users.noreply.github.com>
2022-08-19 16:20:04 +0530

    Add files via upload

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,23 @@
+const functions = require('firebase-functions');
+const Filter = require('bad-words');
 
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const db = admin.firestore();
+
+exports.detectiveEvilUsers = functions.firestore
+    .document('messages/{msgId}')
+    .onCreate(async(doc, ctx) => {
+        const filter = new Filter();
+        const {text, uid} = doc.data();
+
+
+        if(filter.isProfane(text)){
+
+            const cleaned = filter.clean(text);
+            await doc.ref.update({text: `😢 I got BANNED for life for saying... ${cleaned}`});
+
+            await db.collection('bannes').doc(uid).set({});
+        }
+    })
